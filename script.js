75563031791c5f3c8286e297b3fd0825068f6723
stephany221212 <104568205+stephany221212@users.noreply.github.com>
2024-08-23 09:52:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -39,8 +39,73 @@ const perguntas=[
                 afirmacao:" "
             },
             {
-                
+                texto:" "
+                afirmacao:" "
             }
         ]
+    },
+    {
+        enunciado:" "
+        alternativas:[
+            {
+                texto:" "
+                afirmacao:" "
+            },
+            {
+                texto:" "
+                afirmacao:" "
+            }
+        ]
+    },
+    {
+        enunciado:" "
+        alternativas:[
+            {
+                texto:" "
+                afirmacao:" "
+            },
+            {
+                texto:" "
+                afirmacao:" "
+            }
+        ]
+    },
+];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
     }
-]
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSeçecionada) {
+    const afirmacoes = opcaoSeçecionada.afirmacoes;
+    historiaFinal += afirmacoes + " ";
+    atual ++;
+    mostraPergunta();
+}
+
+function mostraResultado () {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
